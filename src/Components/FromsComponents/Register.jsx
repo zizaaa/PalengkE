749169571b20d715bd3f7a58ae30af077efdadad
Loamy28 <rome.angelo15@gmail.com>
2023-06-27 14:40:33 +0800

    minor update Register

--- src/Components/FromsComponents/Register.jsx
+++ src/Components/FromsComponents/Register.jsx
@@ -9,192 +9,65 @@ const env = import.meta.env;
 const URL = env.VITE_REACT_SERVER_URL;
 
 const Register = () => {
-  let isValid = {
-    "firstName":false,
-    "lastName":false,
-    "userName":false,
-    "email":false,
-    "password":false,
-    "confirmPassword":false,
-    "number":false,
-    "address":false
-  }
-
   const navigate = useNavigate();
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [userName, setUserName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
   const [number, setNumber] = useState('');
   const [address, setAddress] = useState('');
-  const [isLoading,setIsLoading] = useState(false)
+  const [errorMessage, setErrorMessage] = useState('');
 
   const SwalE = withReactContent(Swal);
 
   const checkNewUser = async () => {
-    try {
-      const { data } = await axios.get(`${URL}/users`);
-        let saveUser = true;
-        data.map((user) => {
-          if (user.userName === userName) {
-              SwalE.fire({
-                icon: 'error',
-                text: `Username ${userName} already exist`,
-                confirmButtonColor: "#435e39",
-              });
-              setIsLoading(false)
-              saveUser=false;
-          }
-          if(saveUser){  
-            setIsLoading(true)          
-            addUser();
-          }
-
-        });
-
-    } catch (error) {
-      Swal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text: `${error}`,
-      })
-      setIsLoading(false)
-    }
-
+    const { data } = await axios.get(`${URL}/users`);
+    data.map((user) => {
+      console.log(user)
+      if (user.userName === userName) {
+        setErrorMessage(`Username ${userName} already exists`);
+        navigate('/forms/login');
+      }else {
+        sessionStorage.setItem('userId', user._id);
+        navigate('/');
+        location.reload();
+      }
+    });
   };
 
   const addUser = async () => {
-    try {
-      const model = {
-        firstName: firstName,
-        lastName: lastName,
-        userName: userName,
-        email: email,
-        password: password,
-        number: number,
-        address: address
-      };
-      await axios.post(`${URL}/users`, model);
-    } catch (error) {
+    const model = {
+      firstName: firstName,
+      lastName: lastName,
+      userName: userName,
+      email: email,
+      password: password,
+      number: number,
+      address: address
+    };
+    const response = await axios.post(`${URL}/users`, model);
+    if (response) {
       SwalE.fire({
         icon: 'error',
-        title: `${error}`,
+        title: 'Account Already Registered',
         confirmButtonColor: "#435e39",
       });
     }
-
-    try {
-      const { data } = await axios.get(`${URL}/users`);
-      data.map((user) => {
-        console.log(user)
-        if (user.userName === userName) {
-          sessionStorage.setItem('userId', user._id);
-          navigate('/');
-          setIsLoading(false)
-        }
-      });
-    } catch (error) {
-      SwalE.fire({
-        icon: 'error',
-        text: `${error}`,
-        confirmButtonColor: "#435e39",
-      });
-    }
-
   };
 
   const validation = (e) => {
     e.preventDefault();
-    setIsLoading(true)
-
-    const userFirstNameValidation =()=>{
-        let wordInName = firstName.split("");
-    
-        if(wordInName.length >=2){
-            isValid.firstName = true;
-        }else if(wordInName.length < 2){
-            isValid.firstName = false;
-        }
-    }
-        const userLastNameValidation =()=>{
-            let wordInName = lastName.split("");
-        
-            if(wordInName.length >=2){
-                isValid.lastName = true;
-            }else if(wordInName.length < 2){
-                isValid.lastName = false;
-            }
-            
-        }
-    const userNameValidation =()=>{
-        let wordInName = userName.split("");
-    
-        if(wordInName.length >=4){
-            isValid.userName = true;
-        }else if(wordInName.length < 2){
-            isValid.userName = false;
-        }
-        
-    }
-
-    const userEmailValidation =()=>{
-        let hasSign = /[@.]/.test(email);
-
-        if(hasSign){
-            isValid.email = true;
-        }else{
-            isValid.email = false;
-        }
-            
-    }
-
-        const usernumberValidation =()=>{
-            if(number != ''){
-                isValid.number= true;
-            }else{
-              isValid.number= false;
-            }
-                
-        }
-    const useraddValidation =()=>{
-        if(address != ''){
-            isValid.address= true;
-        }else{
-          isValid.address= false;
-        }
-            
-    }
-        const userpassValidation =()=>{
-            if(password === confirmPassword){
-                isValid.password= true;
-                isValid.confirmPassword= true;
-            }else{
-              isValid.password= false;
-              isValid.confirmPassword= false;
-            }
-                
-        }
-    userFirstNameValidation();
-    userLastNameValidation();
-    userNameValidation();
-    userEmailValidation();
-    userpassValidation();
-    usernumberValidation();
-    useraddValidation();
-
-    if (isValid.firstName && isValid.lastName && isValid.userName && isValid.email && isValid.password &&isValid.confirmPassword && isValid.number && isValid.address) {
-      console.log('validated')
-      checkNewUser()
+    if (firstName && lastName && userName && email && password && number && address) {
+      checkNewUser();
+      addUser();
     } else {
       SwalE.fire({
-        icon: 'error',
-        title: 'Opss...',
-        text: 'There\'s an error in your information. Please check the details above.',
+        icon: 'warning',
+        title: 'Incomplete Fields',
+        text: 'Please fill in all fields',
         confirmButtonColor: "#435e39",
       });
-      setIsLoading(false)
     }
   };
 
@@ -208,67 +81,65 @@ const Register = () => {
           <p>Create Account</p>
         </div>
         <form>
+          <div className="firstRw">
+            <input
+              type='text'
+              onChange={(e) => setFirstName(e.target.value)}
+              placeholder='First name'
+              value={firstName}
+              required
+            />
+            <input
+              type='text'
+              onChange={(e) => setLastName(e.target.value)}
+              placeholder='Last name'
+              value={lastName}
+              required
+            />
+          </div>
+          <div className="firstRw">
+            <input
+              type='text'
+              onChange={(e) => setUserName(e.target.value)}
+              placeholder='User name'
+              value={userName}
+              required
+            />
+            <input
+              type='text'
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder='Email'
+              value={email}
+              required
+            />
+          </div>
           <input
-            type='text'
-            onChange={(e) => setFirstName(e.target.value)}
-            placeholder='First name'
-            value={firstName}
-            required
-          />
-          <input
-            type='text'
-            onChange={(e) => setLastName(e.target.value)}
-            placeholder='Last name'
-            value={lastName}
-            required
-          />
-          <input
-            type='text'
-            onChange={(e) => setUserName(e.target.value)}
-            placeholder='User name'
-            value={userName}
-            required
-          />
-          <input
-            type='mail'
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder='Email'
-            value={email}
-            required
-          />
-          <input
-            type='password'
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder='Password'
-            value={password}
-            required
-          />
-          <input
-            type='password'
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            placeholder='Confirm password'
-            value={confirmPassword}
-            required
-          />
-          <input
-            type='text'
-            onChange={(e) => setNumber(e.target.value)}
-            placeholder='Number'
-            value={number}
-            required
-          />
-          <input
+            className="resAdd"
             type='text'
             onChange={(e) => setAddress(e.target.value)}
-            placeholder='Address'
+            placeholder='Residential Address'
             value={address}
             required
           />
-          <div className="bestSeller-spinner custom-spinner">
-            <div className={`${isLoading ? 'spinner-border':'custom-spinner-border'}`} role="status">
-                <button onClick={(e)=>{validation(e)}} className={`${isLoading ? 'visually-hidden':'form-btn'}`} type='button' >Register</button>
-            </div>
+          <div className="firstRw">
+            <input
+              type='password'
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder='Password'
+              value={password}
+              required
+            />
+            <input
+              type='text'
+              onChange={(e) => setNumber(e.target.value)}
+              placeholder='Number'
+              value={number}
+              required
+            />
           </div>
+          
+          {errorMessage && <p>{errorMessage}</p>}
+          <button type='submit' onClick={validation}>Sign Up</button>
         </form>
         <div className='bottom-form'>
           <p>Already have an account?</p>
